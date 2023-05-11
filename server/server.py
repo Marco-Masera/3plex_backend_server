9fd1795b8f25bb1562aabb241270701cf3cf6641
Marco-Masera <mirco.masera@gmail.com>
2023-05-11 21:33:27 +0200

    Dev

--- server/server.py
+++ server/server.py
@@ -42,8 +42,9 @@ def submit_job(token):
     ssRNA_fasta.save(os.path.join(WORKING_DIR_PATH,token) + "/" + secure_filename(ssRNA_fasta.filename))
     dsDNA_fasta.save(os.path.join(WORKING_DIR_PATH,token) + "/" + secure_filename(dsDNA_fasta.filename))
     
-    rna_fn = "".join(ssRNA_fasta.filename.split(".")[:-1])
-    dna_fn = "".join(dsDNA_fasta.filename.split(".")[:-1])
+    rna_fn = ssRNA_fasta.filename.removesuffix(f".{ssRNA_fasta.filename.split('.')[-1]}")
+    dna_fn = dsDNA_fasta.filename.removesuffix(f".{dsDNA_fasta.filename.split('.')[-1]}")
+    
     rule = f"snakemake -p -c1 working_dir/{token}/{rna_fn}__{dna_fn}__output.txt"
     config_ = " --config " + " ".join([f"{key}={triplex_params[key]}" for key in triplex_params.keys()])
     print(rule + " " + config_)
