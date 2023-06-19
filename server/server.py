a28f8e669b6097d7553b3476edadc4d57630c345
Marco-Masera <mirco.masera@gmail.com>
2023-06-19 23:59:00 +0200

    .

--- server/server.py
+++ server/server.py
@@ -12,8 +12,13 @@ import re
 app = Flask(__name__)
 
 #Working dir is where temporary files will be stored before and during being processed
-SNAKEFILE_PATH = os.path.dirname(os.path.realpath(__file__))
-WORKING_DIR_PATH = os.path.join(SNAKEFILE_PATH, "working_dir")
+CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+SNAKEFILE_PATH = os.path.join(CURRENT_PATH, "3plex", "local", "rules", "Snakefile")
+CONFIG_PATH = os.path.join(CURRENT_PATH, "3plex", "local", "config", "config_v1.yaml")
+WORKING_DIR_PATH = os.path.join(CURRENT_PATH, "3plex", "working_dir")
+BIN_PATH = os.path.join(CURRENT_PATH, "3plex", "local", "bin")
+BIOINFOTREE_ROOT = "/home/reference_data/bioinfotree/local/bin/"
+CONDA_ENV_PATH = "/home/imoliner/.conda/envs/3plex_test/"
 #Other params
 SERVER_URL = "http://192.168.186.10:8001"
 #HMAC Secret key. Warning_ keep the key used in production safe
@@ -47,9 +52,7 @@ def parse_triplex_params(form):
     parameter_dict['filter_repeat'] = form['filter_repeat']
     parameter_dict['consecutive_errors'] = form['consecutive_errors']
     parameter_dict['SSTRAND'] = form['SSTRAND']
-    print(".")
     for key in parameter_dict.keys():
-        print("..")
         if (not validate_input_params(parameter_dict[key])):
             raise Exception()
     return parameter_dict
@@ -57,7 +60,7 @@ def parse_triplex_params(form):
 
 def execute_command(cmd, path):
     return_code = -1
-    print(f"Executing task: {cmd}")
+    print(f"\nExecuting task: {cmd}\n")
     try:
         cwd = tempfile.mkdtemp(path)
         with subprocess.Popen(['/bin/bash', '-c', cmd], cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=1) as p:
@@ -91,7 +94,7 @@ def ping_job_failed(token, output_dir, htoken):
 
 def ping_job_succeeded(token, output_dir, ssRNA, dsDNA, htoken):
     tries = 0
-    send_response = f"curl {SERVER_URL}/results/submitresult/{token}/ -F SUMMARY=@{output_dir}/{ssRNA}_ssmasked-{dsDNA}.tpx.summary.gz -F STABILITY=@{output_dir}/{ssRNA}_ssmasked-{dsDNA}.tpx.stability -F PROFILE=@{output_dir}/profile_range.msgpack -F HTOKEN={htoken}"
+    send_response = f"curl {SERVER_URL}/results/submitresult/{token}/ -F SUMMARY=@{output_dir}/{ssRNA}_ssmasked-{dsDNA}.tpx.summary.gz -F STABILITY=@{output_dir}/{ssRNA}_ssmasked-{dsDNA}.tpx.stability.gz -F PROFILE=@{output_dir}/profile_range.msgpack -F SECONDARY_STRUCTURE=@{output_dir}/{ssRNA}_secondary_structure.msgpack -F HTOKEN={htoken}"
     while True:
         r = execute_command(send_response, token)
         if (r == 0):
@@ -125,22 +128,40 @@ def submit_job(token):
 
     ssRNA_fasta.save(output_dir + "/" + secure_filename(ssRNA_fasta.filename))
     dsDNA_fasta.save(output_dir + "/" + secure_filename(dsDNA_fasta.filename))
-    
-    rna_fn = ssRNA_fasta.filename.removesuffix(f".{ssRNA_fasta.filename.split('.')[-1]}")
-    dna_fn = dsDNA_fasta.filename.removesuffix(f".{dsDNA_fasta.filename.split('.')[-1]}")
-    
-    
-    rule=f"snakemake -c1 --slurm --default-resources slurm_partition=low --jobs 1 {output_dir}/{rna_fn}__{dna_fn}__output.txt"
-    config_ = " --config " + " ".join([f"{key}={triplex_params[key]}" for key in triplex_params.keys()])
-    srun_config = f'srun --job-name "{token}" --cpus-per-task=1 --mem=12G --nodelist=node3 --pty  '
 
+    dna_fn = dsDNA_fasta.filename.removesuffix(f".{dsDNA_fasta.filename.split('.')[-1]}")
+
+    #Need to build the command to run 3plex
+    conda_setup = """__conda_setup="$('/opt/conda/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/conda/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/opt/conda/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/conda/miniconda3/bin:$PATH"
+    fi
+fi"""
+    setting_up = f"{conda_setup} \n cd {output_dir} \n conda activate {CONDA_ENV_PATH} \n export PATH={BIN_PATH}:$PATH:{BIOINFOTREE_ROOT} \n cd {output_dir}"
+    #Is ssRNA_fasta compressed?
+    ssRNA_fasta_compressed = False
+    if (ssRNA_fasta.filename.split('.')[-1]=="gz"):
+        ssRNA_fasta_compressed = True
+        rna_fn = ssRNA_fasta.filename.removesuffix(f".{ssRNA_fasta.filename.split('.')[-2]}")
+        setting_up = setting_up + f"\n gzip -d {ssRNA_fasta.filename} "
+    else:
+        rna_fn = ssRNA_fasta.filename.removesuffix(f".{ssRNA_fasta.filename.split('.')[-1]}")
+    
+    link_files = f"\n ln -s {SNAKEFILE_PATH} {output_dir} \n ln -s {CONFIG_PATH} {output_dir}/config.yaml "
+    rule=f"snakemake -c1 --slurm --default-resources slurm_partition=low mem_mb=8000 --jobs 10 {rna_fn}_ssmasked-{dna_fn}.tpx.summary.add_zeros.gz {rna_fn}_ssmasked-{dna_fn}.tpx.stability.gz  {rna_fn}_secondary_structure.msgpack {rna_fn}profile_range.msgpack"
+    config_ = " --config " + " ".join([f"{key}={triplex_params[key]}" for key in triplex_params.keys()])
     response = Response( f"Job with token {token} received" )
     
     @response.call_on_close
     def on_close():
         hashed_token = get_hashed(token)
-
-        return_code = execute_command(f"cd {SNAKEFILE_PATH} \n source env/bin/activate \n bioinfotree \n {rule} {config_} > {output_dir}/STDOUT 2>{output_dir}/STDERR", token)
+        print(f"{setting_up} {link_files} \n {rule} {config_} > {output_dir}/STDOUT 2>{output_dir}/STDERR")
+        return_code = execute_command(f"{setting_up} {link_files} \n {rule} {config_} > {output_dir}/STDOUT 2>{output_dir}/STDERR", token)
         if (return_code==0):
             ping_job_succeeded(token, output_dir, rna_fn, dna_fn, hashed_token)
         else:
