674b0873aaf2abc2b19efbb990309b9a6e64a62d
Marco-Masera <mirco.masera@gmail.com>
2024-05-17 11:48:40 +0200

    .

--- server/server_config.py
+++ server/server_config.py
@@ -5,7 +5,7 @@ from  server_config_local import *
 DELETE_JOB_DIRECTORY_AFTER_SUCCESS = True
 DEBUG_SKIP_SERVER_AUTHENTICATION = False
 #In quiet mode, the server prints only when something goes wrong. Without quiet mode it prints every command it runs.
-QUIET_MODE = False
+QUIET_MODE = True
 
 #RELATIVE PATH VARIABLES - these are unlikely to be changed
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
